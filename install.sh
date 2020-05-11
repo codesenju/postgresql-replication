815b28124589a82b8afcdc09b70fa9656774cfb1
Lehlogonolo <codesenju@gmail.com>
2020-05-11 19:39:56 +0200

    Final changes!

--- install.sh
+++ install.sh
@@ -12,26 +12,24 @@ echo "wait 20 sec…"
 sleep 10 && echo "loading…" && sleep 10
 
 echo "Editing the general configuration files:"
-docker exec -it master-db psql -U postgres -c "CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'passw0rd';"
-
-docker exec -it master-db psql -U postgres -c "ALTER SYSTEM SET listen_addresses TO '*'"
-echo host replication replicator 0.0.0.0/0 trust >> /$PWD/postgres/pg_hba.conf
-
+docker exec -it master-db psql -U postgres -c "CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'passw0rd';" && \
+docker exec -it master-db psql -U postgres -c "ALTER SYSTEM SET listen_addresses TO '*'" && \
+echo host replication replicator 0.0.0.0/0 trust >> /$PWD/postgres/pg_hba.conf && \
 docker exec -it master-db psql -U postgres -c "alter system set wal_level = replica ;" && \
 docker exec -it master-db psql -U postgres -c "alter system set max_wal_senders = 16;" && \
-docker exec -it master-db psql -U postgres -c "alter system set hot_standby = on;"
-
-docker exec -it master-db psql -U postgres -c "select pg_reload_conf()"
+docker exec -it master-db psql -U postgres -c "alter system set hot_standby = on;" && \
+docker exec -it master-db psql -U postgres -c "select pg_reload_conf()" # re-load configuration file
 
 # wait 8 seconds
 sleep 4 && echo "We're almost done..." && sleep 4
 
-echo "master-db setup complete"
+echo "Configuration complete"
 sleep 2
-echo "Now setting up replica-db"
-docker exec -it master-db /bin/bash -c 'pg_basebackup -h master-db -U replicator -p 5432 -D /tmp/postgresslave -Fp -Xs -P -Rv'
 
-docker cp master-db:/tmp/postgresslave /$PWD/
+echo "Now setting up replica-db"
+docker exec -it master-db /bin/bash -c 'pg_basebackup -h master-db -U replicator -p 5432 -D /tmp/postgresslave -Fp -Xs -P -Rv' # make a backup of master-db
+
+docker cp master-db:/tmp/postgresslave /$PWD/ # copy backup data to current directory
 
 docker run --name replica-db -d -p 15433:5432 -e POSTGRES_DB=test -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_USER=postgres -v /$PWD/postgresslave:/var/lib/postgresql/data --net test postgres:latest
 
