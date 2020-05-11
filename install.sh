554dc6459faaa64c1deefbfc94fece341215b7da
Lehlogonolo <l.masubelele@gmail.com>
2020-05-11 18:35:53 +0200

    Final changes

--- install.sh
+++ install.sh
@@ -7,26 +7,18 @@ docker run --name master-db -d -p 5432:5432 --net test -e POSTGRES_DB=test -e PO
 sleep 10
 echo "master-db container running on port 5432"
 
-
-# wait 24 seconds
+# wait 20 seconds
 echo "wait 20 sec…"
 sleep 10 && echo "loading…" && sleep 10
 
+echo "Editing the general configuration files:"
 docker exec -it master-db psql -U postgres -c "CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'passw0rd';"
 
-
 docker exec -it master-db psql -U postgres -c "ALTER SYSTEM SET listen_addresses TO '*'"
-
-
 echo host replication replicator 0.0.0.0/0 trust >> /$PWD/postgres/pg_hba.conf
 
-docker exec -it master-db psql -U postgres -c "select pg_reload_conf()"
-
-echo "Editing the general configuration files:"
 docker exec -it master-db psql -U postgres -c "alter system set wal_level = replica ;" && \
 docker exec -it master-db psql -U postgres -c "alter system set max_wal_senders = 16;" && \
-docker exec -it master-db psql -U postgres -c "alter system set max_wal_size = '200MB';" && \
-docker exec -it master-db psql -U postgres -c "alter system set wal_keep_segments = 8;" && \
 docker exec -it master-db psql -U postgres -c "alter system set hot_standby = on;"
 
 docker exec -it master-db psql -U postgres -c "select pg_reload_conf()"
@@ -50,5 +42,5 @@ echo "replica-db container running on port 5433"
 #TEST
 sleep 2
 echo "Checking health…"
-sleep 8
+sleep 10
 docker exec -it master-db psql -U postgres -c 'select * from pg_stat_replication;'
