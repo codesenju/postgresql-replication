cab2153b6e2f95c204ceceb4f872139eef271a27
Lehlogonolo <l.masubelele@gmail.com>
2020-05-16 20:55:55 +0200

    Made final changes to install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,16 @@ docker network create test
 echo "Created network 'test'"
 docker build -t replication/psql .
 docker run --name master-db -d -p 15432:5432 --net test -e POSTGRES_DB=test -e POSTGRES_HOST_AUTH_METHOD=trust -v /$PWD/postgres:/var/lib/postgresql/data replication/psql
+sleep 5
 echo "master-db container running on port 15432"
+sleep 5
+echo "Wait 20 seconds..."
+sleep 10 && echo "Loading..." && sleep 10
+echo "Starting backup"
+sleep 5
 docker exec -it master-db /bin/bash -c 'pg_basebackup -h master-db -U replicator -p 5432 -D /tmp/postgresslave -Fp -Xs -P -Rv' 
 docker cp master-db:/tmp/postgresslave /$PWD/ # copy backup data to current directory
+echo "Backup Complete!"
 docker run --name replica-db -d -p 15433:5432 -e POSTGRES_DB=test -e POSTGRES_HOST_AUTH_METHOD=trust -v /$PWD/postgresslave:/var/lib/postgresql/data --net test replication/psql
 sleep 5
 echo "replica-db container running on port 15433"
