a2164e356f61f8938d208067f7b46a768daef827
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-19 15:44:54 +0200

    Update setup-db.sh

--- setup-db.sh
+++ setup-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# pg_restore is a utility for restoring a PostgreSQL database from an archive created by pg_dump
-# Here we are restoring tables that have records of movie titles(basics.tar.gz) and actors(actors.tar.gz)
+# pg_restore is a utility for restoring a PostgreSQL database from an archive created by pg_dump.
+# Here we are restoring tables that have records of movie titles(basics.tar.gz) and actors(actors.tar.gz) into the movie database.
 pg_restore -d movie /tables/actors.tar.gz
 pg_restore -d movie /tables/basics.tar.gz
