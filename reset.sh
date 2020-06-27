dc7517772b5470a7eb8b9a1b12522020fe14da80
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-27 21:23:45 +0200

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -2,8 +2,11 @@
 
 docker rm -f master-db
 docker rm -f slave-db
-docker rmi -f psql-12/movie-db
+
 docker network rm mynet
+
+docker rmi -f psql-12/movie-db:${BUILD_NUMBER}
+docker rmi -f psql-12/movie-db:latest
 
 rm -rf postgres
 rm -rf postgresslave
