500f3313ea9547a24cfead18683f4ac2ce486dc2
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-27 21:05:00 +0200

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-rm -rf postgres
-rm -rf postgresslave
+
 docker rm -f master-db
 docker rm -f slave-db
 docker rmi -f psql-12/movie-db
 docker network rm mynet
 
+rm -rf postgres
+rm -rf postgresslave
